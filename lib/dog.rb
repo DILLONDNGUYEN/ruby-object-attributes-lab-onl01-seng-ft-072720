221a5cf1ee8edcb5d87845c8934f5fc03af484e2
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-08-05 06:22:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,6 +2,15 @@ class Dog
   def name = (dog_name)
     @name = dog_name
   end
+  def name
+    @name
+  end
   
-  def
+  def breed = (dog_breed)
+    @breed = dog_breed
+  end
+  def breed
+    @breed
+  end
+  
 end
